d5f4d5a76329028245f43958767a7800ee1512fd
kranthikumar-kkk <kranthikumarkolli243@gmail.com>
2026-07-24 14:13:12 +0530

    Create odd-even.java

---
+++ odd-even.java
@@ -0,0 +1,12 @@
+public  class Main{
+    public static void main(String args[]){
+        int a=46;
+        if(a%2==0)
+        {
+         System.out.println("it is even");
+        }
+        else{
+            System.out.println("it is odd");
+        }
+    }
+}                                                                                                    output: it is a even
